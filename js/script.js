1e05c9cb808c7c27846d2e8de6c1864a13f51607
007Kohaku <mr148980@gmail.com>
2023-12-28 21:15:40 +0700

    day one & error

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Toggle Class Active
+const navbarNav = document.querySelector(".navbar-nav");
+// Hamburger Menu Click
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
